9b4c591c2070fa62800cdeee372c3e13819723eb
hehao <1421771020@qq.com>
2021-09-19 16:44:42 +0800

    创建列表找房筛选组件

---
+++ src/pages/HouseList/components/Filter/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+
+import FilterTitle from '../FilterTitle'
+import FilterPicker from '../FilterPicker'
+import FilterMore from '../FilterMore'
+
+import styles from './index.module.css'
+
+export default class Filter extends Component {
+  render() {
+    return (
+
+
+
+        {/* <FilterPicker />
+        <FilterMore /> */}
+    )
+  }
+}
--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Flex } from 'antd-mobile'
+
+
 import SearchHeader from '../../components/SearchHeader'
+import Filter from './components/Filter'
 import styles from './index.module.css'
 
 const { label } =JSON.parse(localStorage.getItem('hkzf_city'))
@@ -9,10 +12,14 @@ export default class HouseList extends React.Component {
   render() {
     return (
       <div>
+        {/* 搜索导航栏 */}
         <Flex className={styles.header}>
           <i className="iconfont icon-back" onClick={()=>this.props.history.go(-1)}/>
           <SearchHeader cityName={label} className={styles.searchHeader}/>
         </Flex>
+        
+        {/* 条件筛选栏 */}
+        <Filter />
       </div>
     )
   }
